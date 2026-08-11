0593aa42a528a83312c348062b19548f03d541f7
Aishwarya Shrivastava <146877837+Aishhwaryaa-Shrivastava@users.noreply.github.com>
2026-08-11 23:44:06 +0530

    Add files via upload

--- models/arima_model.py
+++ models/arima_model.py
@@ -1,1 +1,7 @@
+from statsmodels.tsa.arima.model import ARIMA
 
+def run_arima(data):
+    model = ARIMA(data, order=(1,1,1))
+    model_fit = model.fit()
+    forecast = model_fit.forecast(steps=10)
+    return forecast
